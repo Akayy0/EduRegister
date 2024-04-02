1e3029439ba6418b1286e4fec0a1514aba1347a9
Akayy0 <joaovitorotoni6@gmail.com>
2024-04-02 20:39:36 -0300

    get mapping

--- api/src/main/java/com/akayy/EduRegister/controller/StudentController.java
+++ api/src/main/java/com/akayy/EduRegister/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.akayy.EduRegister.controller;
 import com.akayy.EduRegister.model.Student;
 import com.akayy.EduRegister.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -20,4 +19,10 @@ public class StudentController {
         return "New student is added";
 
     }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
 }
--- api/src/main/java/com/akayy/EduRegister/service/StudentService.java
+++ api/src/main/java/com/akayy/EduRegister/service/StudentService.java
@@ -2,6 +2,9 @@ package com.akayy.EduRegister.service;
 
 import com.akayy.EduRegister.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
